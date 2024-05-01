d93366c4a9171a7dc9e3514d82bf6c9858a2b120
kazzical <133119885+kazzical@users.noreply.github.com>
2024-05-01 19:52:42 +0000

    practiced about console.log

---
+++ script.js
@@ -0,0 +1,12 @@
+let mood;
+mood = "happy";
+let reccomendedSong;
+
+if (mood === "happy") {
+  console.log("Here Comes the Sun by The Beatles");
+}
+else if (mood === "sad") {
+  console.log("Someone Like You by Adele");
+} else {
+  console.log("Break My Soul by Beyonce");
+}
